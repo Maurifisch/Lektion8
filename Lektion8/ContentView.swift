5289ce04e28f53c37d71dc1c8444f2f6105b9819
Maurifisch <info@fischbein.de>
2023-04-18 13:36:14 +0200

    Lektion8

--- Lektion8/ContentView.swift
+++ Lektion8/ContentView.swift
@@ -8,16 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private  var messageString = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+       // ZStack {
+         //   Rectangle()
+            //    .fill(.black.gradient)
+            
+            //Color.yellow.opacity(0.5)
+               // .ignoresSafeArea()
+            
+        
+            VStack {
+                    //Text("You Have Skills")
+                      //  .font(.largeTitle)
+                       // .fontWeight(.black)
+                        //.foregroundColor(Color.blue)
+                       // .padding()
+                       // .background(Color(.orange))
+                       // .cornerRadius(15)
+                   
+                    Spacer()
+     
+                    
+                    Text(messageString)
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                       .minimumScaleFactor(0.5)
+                       .multilineTextAlignment(.center)
+                        .foregroundColor(.red)
+                        .frame(height: 150)
+                        .frame(maxWidth: .infinity)
+                        //.border(.orange, width: 1)
+                        .padding()
+                    
+                    Spacer()
+                    
+                   
+                    
+                       
+                   HStack {
+                       Button("Awesome") {
+                               messageString = "You Are Awesome!"
+                           }
+                           
+                      // .buttonStyle(.borderedProminent)
+                       Spacer()
+                       
+                       Button("Great"){
+                           messageString = " You Are Great"
+                   }
+                       
+                       
+                       }
+                       .buttonStyle(.borderedProminent)
+                    
+                    
+                       
+                   
+                   
+                       //.border(.purple, width: 5)
+                   // zeichnet lila Rahmen um awesome und great
+                       .padding()
+                       
+            }
         }
-        .padding()
-    }
-}
+        }
+       
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
