f1b85e56b62e0fcf2d106a69380d520df0838f3e
Maurifisch <info@fischbein.de>
2023-04-18 13:35:25 +0200

    Initial Commit

---
+++ Lektion8/Lektion8App.swift
@@ -0,0 +1,17 @@
+//
+//  Lektion8App.swift
+//  Lektion8
+//
+//  Created by Mauri Fischbein on 18.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Lektion8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
